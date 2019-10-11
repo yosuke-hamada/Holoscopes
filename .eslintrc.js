9160e779fcf0cf072d40cd696958030362acbcf0
Yosuke Hamada <yosuke.hamada@a-saas.com>
2019-10-11 15:14:03 +0900

    Feature: テキストインプットコンポーネントを実装した

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'vue/require-default-prop': 'off',
     'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': 'off',
   },
 }
