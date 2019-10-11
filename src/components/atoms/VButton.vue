29873c828f1ee19133799e0b669a611c69bd8d21
Yosuke Hamada <yosuke.hamada@a-saas.com>
2019-10-11 09:33:33 +0900

    Feature: ボタンコンポーネントを作った

---
+++ src/components/atoms/VButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <button class="v-button" :style="styles" @click="onClick()">
+    {{ text }}
+  </button>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Prop } from 'vue/types/options.d'
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String as Prop<string>,
+      required: true,
+    },
+    width: {
+      type: ([String, Number] as any) as Prop<string | number>,
+    },
+    height: {
+      type: ([String, Number] as any) as Prop<string | number>,
+    },
+    color: {
+      type: String as Prop<string>,
+    },
+    backgroundColor: {
+      type: String as Prop<string>,
+    },
+  },
+  computed: {
+    styles(): object {
+      const { width, height, color, backgroundColor } = this
+      return {
+        width: width,
+        height: height,
+        color: color,
+        'background-color': backgroundColor,
+      }
+    },
+  },
+  methods: {
+    onClick(): void {
+      this.$emit('click')
+    },
+  },
+})
+</script>
+<style lang="sass" scoped>
+.v-button
+  border: 0
+  border-radius: 10px
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.12), 0 2px 2px 0 rgba(0,0,0,.24)
+.v-button:hover
+  cursor: pointer
+  box-shadow: 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12), 0 2px 4px -1px rgba(0,0,0,.2)
+.v-button:focus
+  outline: 0
+</style>
