2de4c44e57f4c173b20f7e4f25f4bc788df43f1c
yosuke-hamada <yosuke.hamada@a-saas.com>
2019-10-28 00:15:52 +0900

    Feature: netlify-lamdaを導入した

--- src/libs/api-call.ts
+++ src/libs/api-call.ts
@@ -1,16 +1,13 @@
 import axios from 'axios'
-import * as moment from 'moment'
 import { Horoscope } from '../types/horoscope'
 
 class ApiCall {
-  async fetchResult(): Promise<Array<Horoscope>> {
-    const baseUrl = 'api/horoscope/free/'
-    const today = moment().format('YYYY/MM/DD')
-    const url = `${baseUrl}${today}`
+  async fetchResult(): Promise<Horoscope[]> {
+    const url = '.netlify/functions/horoscope'
     try {
       const response = await axios.get(url)
-      const result = response.data.horoscope[today]
-      return result
+      response.data
+      return response.data
     } catch (error) {
       return error
     }
